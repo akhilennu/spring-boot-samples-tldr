d99bb0206124642c47947f91e7ece69b43b298df
Akhil Ennu <akhil.ennu@github.com>
2025-08-19 16:49:28 +0530

    initial Maven project scaffold with spring initilizr

---
+++ src/main/java/com/akhilennu/samples/tldr/SpringBootSamplesTldrApplication.java
@@ -0,0 +1,13 @@
+package com.akhilennu.samples.tldr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamplesTldrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamplesTldrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhilennu/samples/tldr/SpringBootSamplesTldrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhilennu.samples.tldr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSamplesTldrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
